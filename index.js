1a6fabdaa5a74d0f8022c446716a31b882b6bcb7
Gwachukwu <gwachukwuelebeke@gmail.com>
2020-02-12 14:31:04 +0100

    UPDATE

--- index.js
+++ index.js
@@ -1,21 +1,26 @@
- let randomNum1 = Math.floor(Math.random() * 6 )+ 1; 
- let randomImg = "dice" + randomNum1 + ".png";
- let randomImgSrc = " images/" + randomImg;
- let image = document.querySelectorAll("img")[0];
-image.setAttribute("src", randomImgSrc);   
+document.querySelector("button").addEventListener("click", handleclick);
 
-let randomNum2 = Math.floor(Math.random() * 6 )+ 1; 
- let randomImg2 = "dice" + randomNum2 + ".png";
- let randomImgSrc2 = " images/" + randomImg2;
- let image2 = document.querySelectorAll("img")[1];
-image2.setAttribute("src", randomImgSrc2);
+function handleclick() {
+  //Generate random image for first dice
+  let randomNum1 = Math.floor(Math.random() * 6) + 1;
+  let randomImg = "dice" + randomNum1 + ".png";
+  let randomImgSrc = " images/" + randomImg;
+  let image = document.querySelectorAll("img")[0];
+  image.setAttribute("src", randomImgSrc);
 
+  //Generate random image for second dice
+  let randomNum2 = Math.floor(Math.random() * 6) + 1;
+  let randomImg2 = "dice" + randomNum2 + ".png";
+  let randomImgSrc2 = " images/" + randomImg2;
+  let image2 = document.querySelectorAll("img")[1];
+  image2.setAttribute("src", randomImgSrc2);
 
-if(randomNum1 === randomNum2){
-    document.querySelector("h1").textContent="DRAW!!!";
-}else if(randomNum1 > randomNum2){
-    document.querySelector("h1").textContent="PLAYER 1 WINS!!!";
-}else{
-    document.querySelector("h1").textContent="PLAYER 2 WINS!!!";
-
+  //Declare result
+  if (randomNum1 === randomNum2) {
+    document.querySelector("h1").textContent = "DRAW!!!";
+  } else if (randomNum1 > randomNum2) {
+    document.querySelector("h1").textContent = "PLAYER 1 WINS!!!";
+  } else {
+    document.querySelector("h1").textContent = "PLAYER 2 WINS!!!";
+  }
 };
